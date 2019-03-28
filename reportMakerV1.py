99978084b8e09ce5ce0154960c03b17e59688716
Mat Scull <mscull@gmail.com>
2019-03-28 23:43:33 +0000

    Template basics

--- reportMakerV1.py
+++ reportMakerV1.py
@@ -5,16 +5,21 @@ For test purposes, create 1000 random items in df and plot distribution
 
 Also output percentile values
 
+Templates
+    Layout
+        - header, footer. Consistent css etc
+        - block 1
+        - block 2
+        - block 3
 '''
+
 import argparse
 import jinja2
 import pandas as pd
 import numpy as np
-from flask import render_template
 
 def get_args():
     parser = argparse.ArgumentParser(description = "Make a report")
-    #parser.add_argument('--infile', '-i', default = '')
     parser.add_argument('--debug', '-d', action = 'store_true')
     return parser.parse_args()
 
@@ -25,29 +30,23 @@ def makeDf():
     return df
 
 def makeHtml(df):
-    #templateLoader = jinja2.FileSystemLoader(searchpath="./templates/")
-    #templateEnv = jinja2.Environment(loader=templateLoader)
-    #TEMPLATE_FILE = "main.html"
-    #template = templateEnv.get_template(TEMPLATE_FILE)
-    #return template.render('template.html', my_string="Wheeeee!", my_list=[0,1,2,3,4,5])
-    #t= jinja2.Template('Hello {{ something}}!')
-    return render_template('layout.html',something = 'world')
+    templateLoader = jinja2.FileSystemLoader(searchpath="./templates/")
+    templateEnv = jinja2.Environment(loader=templateLoader)
+    TEMPLATE_FILE = "block1.html"
+    template = templateEnv.get_template(TEMPLATE_FILE)
+    return template.render(df = df, aaa='998i')
 
 def writeFile(filename, html):
     print('Writing file %s' % (filename))
-    print(html)
+    #print(html)
     outfile = open(filename , 'w')
     outfile.write(html)
     outfile.close()
 
 def main():  
     df = makeDf()
-    hist = df.hist()
-
-    html = makeHtml(df)
-    
+    html = makeHtml(df)    
     writeFile('test.html', html)
-
 
 args = get_args()
 main()
